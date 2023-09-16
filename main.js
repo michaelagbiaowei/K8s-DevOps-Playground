81a8eb015ba5d800d662e6ad9e68c0216a6a41bd
michaelagbiaowei <bryanwalton1997@gmail.com>
2023-09-16 20:41:41 +0100

    updated

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!, Welcome to my Web App')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!, Welcome to my Web App");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Application listening on port ${port}`);
+});
